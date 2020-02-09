e89ff07627c654ccfd2f3ace2f38383a32376aa7
Hendi Santika <hendisantika@gmail.com>
2020-02-10 06:33:26 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootoraclestoredproc/SpringbootOracleStoredProcApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoraclestoredproc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOracleStoredProcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOracleStoredProcApplication.class, args);
+    }
+
+}
