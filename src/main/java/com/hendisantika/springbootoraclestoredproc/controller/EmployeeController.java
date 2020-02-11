76159f5cbe6f7fdc5c1aa9e49a1a29c697a74d49
Hendi Santika <hendisantika@gmail.com>
2020-02-11 19:17:32 +0700

    #25 - Add getAllEmployees in EmployeeController

--- src/main/java/com/hendisantika/springbootoraclestoredproc/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/springbootoraclestoredproc/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import com.hendisantika.springbootoraclestoredproc.repository.ProcedureRepositor
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -114,6 +115,13 @@ public class EmployeeController {
                 .lastName(faker.name().lastName())
                 .email(faker.internet().emailAddress())
                 .build();
+    }
+
+    @GetMapping(path = "/employees", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    Iterable<Employee> getAllEmployees() {
+        log.info("Came inside getAllEmployees");
+        return employeeRepository.findAll();
     }
 
 }
